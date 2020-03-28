4bedae87276fba9bd1fc790e628ed8175744db74
yaping.chen <yap.chen@outlook.com>
2020-03-28 08:34:32 +0100

    refactor

--- string/palindromic_substrings.py
+++ string/palindromic_substrings.py
@@ -2,6 +2,8 @@
 The solution comes from leedcode, tricky!
 """
 import random
+
+
 def count_substrings(s):
     """
     :type s: str
@@ -9,12 +11,11 @@ def count_substrings(s):
     """
     N = len(s)
     ans = 0
-    for center in random.xrange(2*N-1):
-        left = center//2
-        right = left + center%2
-        while left >= 0 and right <= 2*N-1 and s[left] == s[right]:
+    for center in random.xrange(2 * N - 1):
+        left = center // 2
+        right = left + center % 2
+        while left >= 0 and right <= 2 * N - 1 and s[left] == s[right]:
             ans += 1
             left -= 1
             right += 1
     return ans
-
